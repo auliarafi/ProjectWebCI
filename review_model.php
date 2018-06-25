b3154c82f561b150c16629942bb3887de1311741
Syarah Fidia Rosa <33827207+syafiraOI@users.noreply.github.com>
2018-06-25 22:46:55 +0700

    model review

---
+++ review_model.php
@@ -0,0 +1,28 @@
+<?php
+class review_model extends CI_Model {
+    function __construct(){
+        parent::__construct();  
+    }
+      
+    function show_data_review(){        
+        $this->db->select('*');
+        $this->db->from('usulan_ta');
+        return $this->db->get(); 
+    }
+
+    public function get_default($NIM){
+        $sql = $this->db->query("SELECT * FROM usulan_ta WHERE NIM = ".intval($NIM));
+        if($sql->num_rows() > 0)
+            return $sql->row_array();
+        return false;
+    }
+
+    public function update($post, $NIM){
+        $review = $this->db->escape($post['review']);
+
+        $sql = $this->db->query("UPDATE usulan_ta SET REVIEW = $review WHERE NIM = ".intval($NIM));
+
+        return true;
+    }
+}
+?>
